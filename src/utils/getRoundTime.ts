91c33aec94c88030e9aa40ecbeb0a614f8c07560
Samuel Pinto <samuel@maispc.com>
2022-05-30 22:10:46 +0100

    Move actions to application and rules to domain

--- src/utils/getRoundTime.ts
+++
@@ -1,7 +0,0 @@
-import { config } from "../config";
-import { isFeatEnabled } from "../services/feat-panel";
-
-const timeout = config.get("timeout");
-const isVIPUser = isFeatEnabled("vip");
-
-export const getRouteTime = () => (isVIPUser ? timeout * 1.25 : timeout);
