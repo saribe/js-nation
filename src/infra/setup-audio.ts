77c3a94da863bd1a586d3750c894b6fe38e43bec
Samuel Pinto <samuel@maispc.com>
2022-06-01 02:50:24 +0100

    chore: correct typo (again 🙈)

--- src/infra/setup-audio.ts
+++ src/infra/setup-audio.ts
@@ -4,7 +4,7 @@ import { isVariant } from "../services/ab-panel";
 import { setupFriendsAudio } from "./setup-friends-audio";
 
 export const setupAudio = (bus: Bus) => {
-  return isVariant("mortal_combat")
+  return isVariant("mortal_kombat")
     ? setupMortalKombatAudio(bus)
     : setupFriendsAudio(bus);
 };
